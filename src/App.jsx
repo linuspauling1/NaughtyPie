b00ac5b1e01fea6227dedf28bcb40722a2fcb5df
linuspauling1 <paulb2233@yahoo.com>
2024-03-12 18:30:06 +0200

    Skeleton

--- src/App.jsx
+++ src/App.jsx
@@ -11,26 +11,47 @@ import Typography from '@mui/material/Typography';
 import Divider from '@mui/material/Divider';
 import ListItem from '@mui/material/ListItem';
 import ListItemButton from '@mui/material/ListItemButton';
-import ListItemIcon from '@mui/material/ListItemIcon';
 import ListItemText from '@mui/material/ListItemText';
 import Button from '@mui/material/Button';
 import IconButton from '@mui/material/IconButton'
 import GitHubIcon from '@mui/icons-material/GitHub';
 import HighlightIcon from '@mui/icons-material/Highlight';
+import GroupIcon from '@mui/icons-material/Group';
+import AppsIcon from '@mui/icons-material/Apps';
+import DevicesOtherIcon from '@mui/icons-material/DevicesOther';
+import AccountCircleIcon from '@mui/icons-material/AccountCircle';
+import ExitToAppIcon from '@mui/icons-material/ExitToApp';
 import { ThemeProvider, createTheme } from '@mui/material/styles';
 import { useState } from 'react';
 
 const drawerWidth = 240;
-
-const lightTheme = createTheme({
-  palette: {
-    mode: 'dark',
-  },
-});
+const iconSx = { fontSize: '1.3rem', marginRight: '0.4rem', };
 
 const darkTheme = createTheme({
   palette: {
+    mode: 'dark',
+    primary: {
+      main: '#3f51b5',
+    },
+    background: {
+      default: '#303030',
+    },
+    drawerBackground: {
+      main: '#424242',
+    },
+  },
+});
+
+
+const lightTheme = createTheme({
+  palette: {
     mode: 'light',
+    primary: {
+      main: '#3f51b5',
+    },
+    drawerBackground: {
+      main: '#ffffff',
+    },
   },
 });
 
@@ -43,7 +64,7 @@ export default function App() {
     <ThemeProvider theme={theme}>
       <Box sx={{ display: 'flex' }}>
         <CssBaseline />
-        <AppBar position="fixed" sx={{ zIndex: (theme) => theme.zIndex.drawer + 1 }}>
+        <AppBar position="fixed" sx={{ zIndex: (theme) => theme.zIndex.drawer + 1, }}>
           <Toolbar>
             <Typography variant="h5" sx={{ cursor: 'pointer', userSelect: 'none' }}>
               Naughty pie
@@ -51,6 +72,24 @@ export default function App() {
             <Typography sx={{ marginLeft: '0.8rem', flexGrow: 1, cursor: 'pointer', userSelect: 'none' }}>
               @0.0.0
             </Typography>
+            {[{text: 'users', icon: <GroupIcon sx={iconSx} />}, 
+              {text: 'apps', icon: <AppsIcon sx={iconSx} />}, 
+              {text: 'clients', icon: <DevicesOtherIcon sx={iconSx} />}, 
+              {text: 'admin', icon: <AccountCircleIcon sx={iconSx} />},
+              {text: 'logout', icon: <ExitToAppIcon sx={iconSx} />}].map((element)=>(
+              <Button 
+                variant='primary'
+                sx={{
+                  '&:focus': {
+                    outline: 'none',
+                  },
+                  padding: '0.4rem',
+                }}
+              >
+                {element.icon}
+                {element.text}
+              </Button>
+            ))}
             <IconButton onClick={toggleTheme} sx={{ 
               color: 'inherit',
               '&:focus': {
@@ -59,7 +98,9 @@ export default function App() {
               }}>
               <HighlightIcon sx={{ margin: '3px', fontSize: '1.6rem' }} />
             </IconButton>
-            <IconButton sx={{ 
+            <IconButton
+            onClick={() => window.open('https://github.com/linuspauling1/NaughtyPie')} 
+            sx={{ 
               color: 'inherit',
               '&:focus': {
                 outline: 'none',
@@ -74,14 +115,18 @@ export default function App() {
           sx={{
             width: drawerWidth,
             flexShrink: 0,
-            [`& .MuiDrawer-paper`]: { width: drawerWidth, boxSizing: 'border-box' },
+            [`& .MuiDrawer-paper`]: { 
+              width: drawerWidth, 
+              boxSizing: 'border-box',
+              backgroundColor: theme.palette.drawerBackground.main,
+            },
           }}
         >
           <Toolbar />
           <Box sx={{ 
             overflow: 'auto',
             display: 'flex',
-            flexDirection: 'column' 
+            flexDirection: 'column',
             }}
           >
             <List sx={{ padding: '0' }}>
@@ -91,7 +136,7 @@ export default function App() {
             </List>
             <Divider />
             <List sx={{ padding: '0' }}>
-              {['A Raspberry PI', 'Some server'].map((text, index) => (
+              {['A Raspberry PI', 'Some server'].map((text) => (
                 <ListItem key={text} disablePadding>
                   <ListItemButton>
                     <ListItemText primary={text} />
@@ -116,6 +161,16 @@ export default function App() {
             </Button>
           </Box>
         </Drawer>
+        <Button
+          variant='contained'
+          sx={{ 
+            '&:focus': {
+              outline: 'none',
+            }, 
+          }}
+        >
+          Muie
+        </Button>
       </Box>
     </ThemeProvider>
   );
