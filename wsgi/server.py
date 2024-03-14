4038413f2abbf4c0b24f52ec0fad5ebcae27323b
linuspauling1 <paulb2233@yahoo.com>
2024-03-15 01:10:10 +0200

    Added database

--- wsgi/server.py
+++ wsgi/server.py
@@ -1,12 +1,33 @@
 from flask import Flask
-from flask_cors import CORS
 from flask_socketio import SocketIO
 import datetime
 from time import sleep
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
-CORS(app)
 socketio = SocketIO(app, cors_allowed_origins='*')
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///tables.db'
+db = SQLAlchemy(app)
+
+class User(db.Model):
+    __tablename__ = 'user'
+    username = db.Column(db.String(50), primary_key=True)
+    passcode = db.Column(db.String(50), nullable=False)
+    machines = db.Column(db.String(50))
+    #apps = db.relationship('App', backref='user')
+class App(db.Model):
+    __tablename__ = 'app'
+    appcode = db.Column(db.String(50), primary_key=True)
+    appname = db.Column(db.String(50), nullable=False)
+    appuser = db.Column(db.String(50), db.ForeignKey('user.username'))
+    #notifications = db.relationship('Notification', backref='app')
+class Notififaction(db.Model):
+    __tablename__ = 'notification'
+    index = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    created_at = db.Column(db.DateTime, default=datetime.datetime.now())
+    title = db.Column(db.String(50), nullable=False)
+    content = db.Column(db.String(100))
+    appnotified = db.Column(db.String(50), db.ForeignKey('app.appcode'))
 
 def update_datetime():
     while True:
@@ -18,6 +39,12 @@ def update_datetime():
 def hello_world():
     print('Client connected!')
     socketio.start_background_task(update_datetime)
+    db.create_all()
+    admin = User(username='admin', passcode='admin')
+    db.session.add(admin)
+    db.session.commit()
+    all_users = User.query.all()
+    print(all_users)
 
 @socketio.on('disconnect')
 def bye_bye():
